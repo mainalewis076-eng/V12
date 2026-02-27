0f0ac7ccd8c7bebd301fb6a13f920e7edd6d2b92
mainalewis076-eng <mainalewis076@gmail.com>
2026-02-27 14:58:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const app = express();
 // Serve static files
 app.use(express.static(__dirname));
 
-// For SPA - redirect all routes to index.html
+// SPA fallback
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
