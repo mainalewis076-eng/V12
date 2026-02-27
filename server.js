a68ecbccd63db5277222caa565b4eedfb333d901
mainalewis076-eng <mainalewis076@gmail.com>
2026-02-27 14:22:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`✅ Frontend server running on port ${PORT}`);
 });
